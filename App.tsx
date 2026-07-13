26eaf7c822deca70a6455cc601394200960c6dfd
mamouda828-beep <mamouda828@gmail.com>
2026-07-13 19:01:12 +0300

    Set base path for deployment in Vite config

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>SaudiLegal.ai</h1>
+      <p>منصة الذكاء الاصطناعي القانونية</p>
+    </div>
+  );
+}
+
+export default App;
